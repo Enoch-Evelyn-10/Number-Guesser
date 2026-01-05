3512e636290bf340a97ccbe24252d3b77bf11ac6
Enoch-Evelyn-10 <Nuckychecksin@gmail.com>
2026-01-04 22:05:45 -0500

    takes and prints users input converting it to an integer

---
+++ main.py
@@ -0,0 +1,10 @@
+#Number Guessing Game
+#User will pick  a number 1-20 
+# Find a way to have a random number generated
+# Check for and save user input
+#Compare user input to generated number 
+#If matched print correct if not print incorrect 
+
+#record user input
+number= int(input("Choose a number from 1-20: "))
+print(number)
